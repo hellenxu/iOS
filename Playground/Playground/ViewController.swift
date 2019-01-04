bf56df479c24b6400722d9c43ea3d13e90c4022e
hellenxu <heavensxu@gmail.com>
2019-01-03 22:38:35 -0500

    Add: empty project init.

---
+++ Playground/Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playground
+//
+//  Created by Hellen on 2019-01-03.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
