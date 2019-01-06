6d1d6bb295d86ac9aeafd78af34ff2ae16664c20
hellenxu <heavensxu@gmail.com>
2019-01-05 20:55:44 -0500

    Add: first alert dialog.

--- Playground/Playground/ViewController.swift
+++ Playground/Playground/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title:"TestTitle", message:"TestMessage", preferredStyle:.alert)
+        
+        let action = UIAlertAction(title:"ActionOK", style:.default, handler:nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
