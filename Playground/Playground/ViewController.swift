e2e2220b9fcef5d9c23f4fcd04f83e01ad37e36e
hellenxu <heavensxu@gmail.com>
2019-01-08 21:33:43 -0500

    Modified: update target and round value.

--- Playground/Playground/ViewController.swift
+++ Playground/Playground/ViewController.swift
@@ -10,26 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 0
+    var targetValue: Int = 0
+    var roundValue: Int = 1000
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = Int(roundf(slider.value))
+
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let msg = "Current percentage is: \(currentValue)"
+        let msg = "Current percentage is: \(currentValue) \nTarget percentage is: \(targetValue)"
         let alert = UIAlertController(title:"Result", message:msg, preferredStyle:.alert)
         
         let action = UIAlertAction(title:"OK", style:.default, handler:nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
 //        print("Current percentage is: \(slider.value)")
         currentValue = Int(roundf(slider.value))
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        roundValue -= 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        roundLabel.text = String(roundValue)
     }
 }
 
