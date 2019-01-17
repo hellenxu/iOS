2f37421b99f1bc5aa9af113782cdece0da533676
hellenxu <heavensxu@gmail.com>
2019-01-16 21:11:37 -0500

    Add: daily project of iOS.

---
+++ iDailyProject/iDailyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDailyProject
+//
+//  Created by Hellen on 2019-01-16.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
