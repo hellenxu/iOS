2a98fbd58c4b2ba142c7334a5589e210d1b2fdbd
hellenxu <heavensxu@gmail.com>
2019-01-22 21:18:07 -0500

    Add: create an oc project.

---
+++ OCiDaily/OCiDaily/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCiDaily
+//
+//  Created by Hellen on 2019-01-22.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
