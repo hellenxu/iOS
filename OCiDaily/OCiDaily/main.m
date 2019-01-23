1b0114993d457b4dc4f1f08b17fdb72e60d1ade3
hellenxu <heavensxu@gmail.com>
2019-01-22 22:15:05 -0500

    Add: primitive data type basics1.

--- OCiDaily/OCiDaily/main.m
+++ OCiDaily/OCiDaily/main.m
@@ -12,6 +12,14 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int sum = 10 + 20;
         NSLog(@"The sum of 10 and 20 is %i", sum);
+        NSLog(@"Color is %#x", 0xFFEF0D);
+        NSLog(@"integerVar = %i", 50);
+        NSLog(@"floatVar = %f", 331.98);
+        NSLog(@"doubleVar = %e", 8.44e+11);
+        NSLog(@"doubleVar = %g", 8.44e+11);
+        NSLog(@"charVar = %c", 'W');
+        NSLog(@"%c, %c", 120, 121);
+        NSLog(@"%i, %i", 120, 121);
     }
     
     return 0;
