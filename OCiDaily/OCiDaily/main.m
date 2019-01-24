c4472a09a78643407fa9956769b80d9fce243e2f
hellenxu <heavensxu@gmail.com>
2019-01-23 22:50:49 -0500

    Add: expression basic part three.

--- OCiDaily/OCiDaily/main.m
+++ OCiDaily/OCiDaily/main.m
@@ -60,6 +60,14 @@ int main(int argc, const char * argv[]) {
         f2 = i2 / 100.0;
         NSLog(@"%i divided by 100.0 produces %f", i2, f2);
 
+        //expression basic 5
+        unsigned int w1 = 0xA0A0A0A0, w2 = 0xFFFF0000, w3 = 0x00007777;
+        NSLog(@"%x %x %x", w1 & w2, w1 | w2, w1 ^ w2);
+        NSLog(@"%x %x %x", ~w1, ~w2, ~w3);
+        NSLog(@"%x %x %x", w1 & ~w2, w1 | w2 | w3, w1 ^ w1);
+        NSLog(@"%x %x", (w1 | w2) & w3, (w1 | w2) & ~ w3);
+        NSLog(@"%x %x", ~(~w1 & ~w2), ~(~w1 | ~w2));
+
     }
     
     return 0;
