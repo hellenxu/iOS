ef9d5acba48224762ea367ececfa98420fc16338
hellenxu <heavensxu@gmail.com>
2019-01-23 22:26:46 -0500

    Add: expression basic part one.

--- OCiDaily/OCiDaily/main.m
+++ OCiDaily/OCiDaily/main.m
@@ -20,6 +20,27 @@ int main(int argc, const char * argv[]) {
         NSLog(@"charVar = %c", 'W');
         NSLog(@"%c, %c", 120, 121);
         NSLog(@"%i, %i", 120, 121);
+        
+        //expression basic 1
+        int a = 100;
+        int b = 2;
+        int c = 20;
+        int d = 4;
+        
+        NSLog(@"a - b = %i", a - b);
+        NSLog(@"b * c = %i", b * c);
+        NSLog(@"a / b = %i", a / c);
+        NSLog(@"a * b + c * d = %i", a * b + c * d);
+
+        //expression basic 2
+        a = 25;
+        float e = 25.0;
+        float f = 2.0;
+        NSLog(@"6 + a / 5 * b = %i", 6 + a / 5 * b);
+        NSLog(@"a / b * b = %i", a / b * b);
+        NSLog(@"e / f * f = %f", e / f * f);
+        NSLog(@"-a = %i", -a);
+
     }
     
     return 0;
