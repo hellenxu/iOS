39260b55dabde92d90b9df098944ca663a691a3d
hellenxu <heavensxu@gmail.com>
2019-01-24 22:58:01 -0500

    Add: swift basic - control flow.

--- SfDaily/SfDaily/main.swift
+++ SfDaily/SfDaily/main.swift
@@ -41,3 +41,42 @@ for codeUnit in dogString.utf8 {
 }
 print("\n")
 
+
+//control flow - for-in loop
+let individualScores = [74, 56, 90, 100, 400]
+var bonusScore = 0
+for score in individualScores {
+    if(score > 90) {
+        bonusScore += 10
+    } else {
+        bonusScore -= 5
+    }
+}
+print("Your bonus score is \(bonusScore)")
+
+//control flow - iteration of dictionary
+let interestingNumbers = [
+    "Prime": [1, 2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8, 13, 21],
+    "Square": [1, 4, 9, 16, 25, 36, 49]
+]
+var max = 0
+for(_, numbers) in interestingNumbers {
+    for num in numbers {
+        if(num > max) {
+            max = num
+        }
+    }
+}
+print("max is \(max)")
+
+//control flow - switch
+let someCharacter = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet.")
+case "z":
+    print("The last letter of the alphabet.")
+default:
+    print("Some other character")
+}
