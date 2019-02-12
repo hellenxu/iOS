6cf8655ee56cfd717700b4c138826323e1a11045
hellenxu <heavensxu@gmail.com>
2019-02-11 20:31:35 -0500

    Add: getFlower code.

--- OCiDailyProject/OCiDailyProject/ViewController.m
+++ OCiDailyProject/OCiDailyProject/ViewController.m
@@ -7,12 +7,51 @@
 //
 
 #import "ViewController.h"
+#import "WebKit/WebKit.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    IBOutlet UISegmentedControl *colorChoice;
+    IBOutlet UILabel *chosenColor;
+    IBOutlet WKWebView *flowerView;
+
+}
 
 @end
 
 @implementation ViewController
+
+- (IBAction)getFlower:(id)sender {
+    NSString *outputHtml;
+    NSString *color;
+    NSString *colorVal;
+    int colorNum;
+    colorNum = colorChoice.selectedSegmentIndex;
+    
+    switch (colorNum) {
+        case 0:
+            color = @"Red";
+            colorVal = @"red";
+            break;
+        case 1:
+            color = @"Blue";
+            colorVal = @"blue";
+            break;
+        case 2:
+            color = @"Yellow";
+            colorVal = @"yellow";
+            break;
+        case 3:
+            color = @"Green";
+            colorVal = @"green";
+            break;
+        default:
+            break;
+    }
+    
+    chosenColor.text = [[NSString alloc] initWithFormat: @"%@", color];
+    outputHtml = [[NSString alloc] initWithFormat:@"<body style='margin: 0px; padding: 0px'> <img height='1200' src='https://teachyourselfios.info/?hour=5&color=%@'></body>", colorVal];
+    [flowerView loadHTMLString:outputHtml baseURL:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
