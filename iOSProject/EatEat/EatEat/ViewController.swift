0f897e8c8d2f864108e18454634619f75fa11cbf
hellenxu <heavensxu@gmail.com>
2019-03-07 21:17:03 -0500

    Add: create a new demo project.

---
+++ iOSProject/EatEat/EatEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EatEat
+//
+//  Created by Hellen on 2019-03-07.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
