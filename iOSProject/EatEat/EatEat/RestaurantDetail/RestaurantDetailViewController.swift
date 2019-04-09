256fc7d11a258991a7d6953b3e485f84cc1fa86c
hellenxu <heavensxu@gmail.com>
2019-04-08 21:30:01 -0400

    Modified: fixed ui warning.

--- iOSProject/EatEat/EatEat/RestaurantDetail/RestaurantDetailViewController.swift
+++ iOSProject/EatEat/EatEat/RestaurantDetail/RestaurantDetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class RestaurantDetailViewController: UITableViewController {
     
@@ -19,7 +21,7 @@ class RestaurantDetailViewController: UITableViewController {
     @IBOutlet weak var lblHeaderAddress: UILabel!
     
     //Cell two
-    @IBOutlet weak var lblDetails: UILabel!
+    @IBOutlet weak var lblTableDetails: UILabel!
     
     //Cell three
     @IBOutlet weak var lblOverallRating: UILabel!
@@ -28,14 +30,89 @@ class RestaurantDetailViewController: UITableViewController {
     @IBOutlet weak var lblAddress: UILabel!
     @IBOutlet weak var imgMap: UIImageView!
     
-    
     var selectedRestaurant: RestaurantItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        dump(selectedRestaurant)
+        initialize()
+    }
+}
+
+private extension RestaurantDetailViewController {
+    func initialize() {
+        setupLabels()
+//        createMap()
     }
     
-
+    func setupLabels() {
+        guard let restaurant = selectedRestaurant else {return }
+        
+        if let name = restaurant.name {
+            lblName.text = name
+            title = name
+        }
+        
+        if let cuisine = restaurant.subtitle {lblCuisines.text = cuisine}
+        
+        if let address = restaurant.address {
+            lblAddress.text = address
+            lblHeaderAddress.text = address
+        }
+        
+        lblTableDetails.text = "Table for 7, tonight at 10:00 PM"
+    }
+    
+    func createMap() {
+        guard let annotation = selectedRestaurant, let lng = annotation.long, let lat = annotation.lat else {return}
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        
+        takeSnapShot(with: location)
+    }
+    
+    func takeSnapShot(with location: CLLocationCoordinate2D) {
+        let mapSnapshotOptions = MKMapSnapshotter.Options()
+        var loc = location
+        let polyline = MKPolyline(coordinates: &loc, count: 1)
+        let region = MKCoordinateRegion(polyline.boundingMapRect)
+        
+        mapSnapshotOptions.region = region
+        mapSnapshotOptions.scale = UIScreen.main.scale
+        mapSnapshotOptions.size = CGSize(width: 340, height: 208)
+        mapSnapshotOptions.showsBuildings = true
+        mapSnapshotOptions.showsPointsOfInterest = true
+        
+        let snapShotter = MKMapSnapshotter(options: mapSnapshotOptions)
+        snapShotter.start() {snapshot, error in
+            guard let snapshot = snapshot else {return }
+            
+            UIGraphicsBeginImageContextWithOptions(mapSnapshotOptions.size, true, 0)
+            snapshot.image.draw(at: .zero)
+            
+            let identifier = "custompin"
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            pinView.image = UIImage(named: "custom-annotation")!
+            let pinImage = pinView.image
+            var point = snapshot.point(for: location)
+            let rect = self.imgMap.bounds
+            if rect.contains(point) {
+                let pinCenterOffset = pinView.centerOffset
+                point.x -= pinView.bounds.size.width / 2
+                point.y -= pinView.bounds.size.height / 2
+                point.x += pinCenterOffset.x
+                point.y += pinCenterOffset.y
+                pinImage?.draw(at: point)
+            }
+            
+            if let image = UIGraphicsGetImageFromCurrentImageContext() {
+                UIGraphicsEndImageContext()
+                DispatchQueue.main.async {
+                    self.imgMap.image = image
+                }
+            }
+        }
+        
+    }
 }
