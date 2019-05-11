d180568bd93994b1fa2f5d804856a9de7dfd9216
hellenxu <heavensxu@gmail.com>
2019-05-10 22:29:59 -0400

    Added: setup restaurant message cell.

---
+++ iOSProject/EatEat/MessageApp/RestaurantMessageCell.swift
@@ -0,0 +1,15 @@
+//
+//  RestaurantMessageCell.swift
+//  MessageApp
+//
+//  Created by Hellen on 2019-05-10.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantMessageCell: UICollectionViewCell {
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblCity: UILabel!
+    @IBOutlet var lblCuisine: UILabel!
+}
