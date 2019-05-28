e852fe6f2a5b7c5722721f5486a868da495edce3
hellenxu <heavensxu@gmail.com>
2019-05-27 22:17:55 -0400

    Modified: remove ambiguous constraints warning.

--- iOSProject/iDailyProject/iDailyProject/ViewController.swift
+++ iOSProject/iDailyProject/iDailyProject/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 60
         requestContactPermission()
     }
 }
