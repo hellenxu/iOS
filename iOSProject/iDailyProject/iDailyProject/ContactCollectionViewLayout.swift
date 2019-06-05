4c6e2e1227681e771d05b9f0fb046697a3368447
hellenxu <heavensxu@gmail.com>
2019-06-04 22:43:44 -0400

    Added: custom collectionview layout - init.

---
+++ iOSProject/iDailyProject/iDailyProject/ContactCollectionViewLayout.swift
@@ -0,0 +1,16 @@
+//
+//  ContactCollectionViewLayout.swift
+//  iDailyProject
+//
+//  Created by Hellen on 2019-06-04.
+//  Copyright © 2019 Six. All rights reserved.
+//
+
+import UIKit
+
+class ContactCollectionViewLayout: UICollectionViewLayout {
+    override func prepare() {
+        
+    }
+    
+}
