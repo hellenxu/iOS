32badcdcd588e0cc47ac2e6bbaf3bad1127c9c18
hellenxu <heavensxu@gmail.com>
2019-06-05 21:56:51 -0400

    Added: calculate cell size and position.

--- iOSProject/iDailyProject/iDailyProject/ContactCollectionViewLayout.swift
+++ iOSProject/iDailyProject/iDailyProject/ContactCollectionViewLayout.swift
@@ -9,8 +9,49 @@
 import UIKit
 
 class ContactCollectionViewLayout: UICollectionViewLayout {
-    override func prepare() {
-        
+    private let itemSize = CGSize(width: 100, height: 90)
+    private let itemSpacing: CGFloat = 10
+    private var layoutAttributes = [UICollectionViewLayoutAttributes]()
+    
+    override var collectionViewContentSize: CGSize {
+        return .zero
     }
     
+    override func prepare() {
+        guard let collectionView = self.collectionView else { return }
+        let availableHeight = Int(collectionView.bounds.height + itemSpacing)
+        let itemHeight = Int(itemSize.height + itemSpacing)
+        let numberOfItems = collectionView.numberOfItems(inSection: 0)
+        let numberOfRows = availableHeight / itemHeight
+        let numberOfColums = Int(ceil(CGFloat(numberOfItems) / CGFloat(numberOfRows)))
+        
+        layoutAttributes.removeAll()
+        layoutAttributes = (0..<numberOfItems).map{index in
+            let row = index % numberOfRows
+            let column = index / numberOfRows
+            
+            var xPos = column * Int(itemSize.width + itemSpacing)
+            if row % 2 == 1 {
+                xPos += Int(itemSize.width / 2)
+            }
+            let yPos = row * Int(itemSize.height + itemSpacing)
+            
+            let indexPath = IndexPath(row: index, section: 0)
+            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            attributes.frame = CGRect(x: CGFloat(xPos), y: CGFloat(yPos), width: itemSize.width, height: itemSize.height)
+            return attributes
+        }
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return false
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return nil
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return nil
+    }
 }
