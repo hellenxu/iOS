563ff011abbaf5758b651a2d621f4c785e74ee81
hellenxu <heavensxu@gmail.com>
2019-07-02 22:19:57 -0400

    fix incorrect column and row numbers.

--- iOSProject/iDailyProject/iDailyProject/ContactCollectionViewLayout.swift
+++ iOSProject/iDailyProject/iDailyProject/ContactCollectionViewLayout.swift
@@ -22,11 +22,11 @@ class ContactCollectionViewLayout: UICollectionViewLayout {
     
     override func prepare() {
         guard let collectionView = self.collectionView else { return }
-        let availableHeight = Int(collectionView.bounds.height + itemSpacing)
-        let itemHeight = Int(itemSize.height + itemSpacing)
+        let availableWidth = Int(collectionView.bounds.width - collectionView.contentInset.left - collectionView.contentInset.right + itemSpacing)
+        let itemWidth = Int(itemSize.width + itemSpacing)
         let numberOfItems = collectionView.numberOfItems(inSection: 0)
-        numberOfRows = availableHeight / itemHeight
-        numberOfColumns = Int(ceil(CGFloat(numberOfItems) / CGFloat(numberOfRows)))
+        numberOfColumns = availableWidth / itemWidth
+        numberOfRows = Int(ceil(CGFloat(numberOfItems) / CGFloat(numberOfColumns)))
         
         layoutAttributes.removeAll()
         layoutAttributes = (0..<numberOfItems).map{index in
@@ -35,7 +35,7 @@ class ContactCollectionViewLayout: UICollectionViewLayout {
             
             var xPos = column * Int(itemSize.width + itemSpacing)
             if row % 2 == 1 {
-                xPos += Int(itemSize.width / 2)
+                xPos += Int(itemSize.width / 3)
             }
             let yPos = row * Int(itemSize.height + itemSpacing)
             
