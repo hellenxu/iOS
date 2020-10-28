ec1f2f95fee5f5922aab5285e2e329cde0ecde58
hellenxu <heavensxu@gmail.com>
2020-10-27 20:44:58 -0400

    add: CustomPresentedViewController.

---
+++ iOSProject/iDailyProject/iDailyProject/CustomPresentedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomPresentedViewController.swift
+//  iDailyProject
+//
+//  Created by Hellen on 2020-10-27.
+//  Copyright © 2020 Six. All rights reserved.
+//
+
+import UIKit
+
+class CustomPresentedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
