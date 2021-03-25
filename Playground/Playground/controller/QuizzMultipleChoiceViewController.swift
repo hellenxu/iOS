7035e33d5601e6cbe2a0aae60c62f05d77546d4e
hellenxu <heavensxu@gmail.com>
2021-03-24 23:48:39 -0400

    add: ui for multiple choices quizz.

---
+++ Playground/Playground/controller/QuizzMultipleChoiceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuizzMultipleChoiceViewController.swift
+//  Playground
+//
+//  Created by Hellen on 2021-03-24.
+//  Copyright © 2021 Six. All rights reserved.
+//
+
+import UIKit
+
+class QuizzMultipleChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
