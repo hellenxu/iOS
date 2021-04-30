966034a989e3860bee4cfd3e36ef9b2235617bd2
hellenxu <heavensxu@gmail.com>
2021-04-29 23:56:39 -0400

    add: viewcontroller for result.

---
+++ Playground/Playground/controller/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  Playground
+//
+//  Created by Hellen on 2021-04-29.
+//  Copyright © 2021 Six. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var recalculateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func onRecalculate(_ sender: UIButton) {
+    }
+    
+}
