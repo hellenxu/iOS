fa4e4b95635c501263cf7f698b668400e04aa033
hellenxu <heavensxu@gmail.com>
2021-05-03 23:27:27 -0400

    add: bmi struct.

--- Playground/Playground/controller/ResultViewController.swift
+++ Playground/Playground/controller/ResultViewController.swift
@@ -14,12 +14,15 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var adviceLabel: UILabel!
     @IBOutlet weak var recalculateButton: UIButton!
     
-    var bmiResult: Float = 0.0
+    var bmiResult: BMI?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bmiLabel.text = String(format: "%.1f", bmiResult)
+        bmiLabel.text = String(format: "%.1f", bmiResult?.value ?? 0.0)
+        adviceLabel.text = bmiResult?.advice
+        view.backgroundColor = bmiResult?.backgroundColor
+        
     }
     
     @IBAction func onRecalculate(_ sender: UIButton) {
--- Playground/Playground/model/BmiCalculator.swift
+++ Playground/Playground/model/BmiCalculator.swift
@@ -6,11 +6,33 @@
 //  Copyright © 2021 Six. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct BmiCalculator {
+    var bmi: BMI?
     
-    func calculateBmi(weight: Float, height: Float) -> Float {
-        return weight / pow(height, 2)
+    func calculateBmi(weight: Float, height: Float) -> BMI {
+        let bmiValue = weight / pow(height, 2)
+        let bmiAdvice: String
+        let bgColor: UIColor
+        if bmiValue < 18.5 {
+            bgColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            bmiAdvice = "Eat more meat"
+        } else if bmiValue <= 24.9 {
+            bgColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+            bmiAdvice = "Fit as model"
+            print("normal")
+        } else {
+            bgColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+            bmiAdvice = "Eat less meat"
+        }
+        
+        return BMI(value: bmiValue, advice: bmiAdvice, backgroundColor: bgColor)
     }
+}
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let backgroundColor: UIColor
 }
