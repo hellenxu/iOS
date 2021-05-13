608299085696873df002d42ce28e206ebfac365e
hellenxu <heavensxu@gmail.com>
2021-05-13 00:32:59 -0400

    add placeholders in vc.

--- Playground/Playground/controller/WeatherViewController.swift
+++ Playground/Playground/controller/WeatherViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var locationBtn: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchBtn: UIButton!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onLocationClicked(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func onSearchClicked(_ sender: UIButton) {
+    }
+    
 }
