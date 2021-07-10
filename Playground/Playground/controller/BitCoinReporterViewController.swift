0bd5df070dd7e67d88d1cb0a1df2dc7a67ea6ace
hellenxu <heavensxu@gmail.com>
2021-07-09 23:59:52 -0400

    add: coin view controller.

--- Playground/Playground/controller/BitCoinReporterViewController.swift
+++ Playground/Playground/controller/BitCoinReporterViewController.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 class BitCoinReporterViewController: UIViewController {
-
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var currencyUnitLabel: UILabel!
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        currencyPickerView.dataSource = self
+        currencyPickerView.delegate = self
+    }
+}
+
+extension BitCoinReporterViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
     }
-    */
+}
 
+extension BitCoinReporterViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        print("xxl-selected: \(selectedCurrency)")
+        let value = coinManager.getCoinValue(currencyUnit: selectedCurrency)
+        valueLabel.text = value
+        currencyUnitLabel.text = selectedCurrency
+    }
 }
---
+++ Playground/Playground/model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  Playground
+//
+//  Created by Hellen on 2021-07-09.
+//  Copyright © 2021 Six. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager{
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseUrl = ""
+    
+    func getCoinValue(currencyUnit: String) -> String {
+        return "900.20"
+    }
+}
